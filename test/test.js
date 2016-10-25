afbbd134d689bf9e8073677c277fcee3f190a3d6
Frederic Cambus <fred@statdns.com>
2016-10-25 09:29:23 +0200

    Reformatting: spaces => tabs

--- test/test.js
+++ test/test.js
@@ -2,19 +2,19 @@ var alexa = require('../lib/alexarank');
 var should = require('should');
 
 describe('alexarank', function() {
-    it('Should return an object with correct elements types', function(done) {
-        alexa("http://www.echojs.com", function(error, result) {
-            should.not.exist(error);
+	it('Should return an object with correct elements types', function(done) {
+		alexa("http://www.echojs.com", function(error, result) {
+			should.not.exist(error);
 
-            result.should.be.an.Object;
+			result.should.be.an.Object;
 
-            result.url.should.equal('echojs.com/');
-            result.idn.should.equal('echojs.com/');
+			result.url.should.equal('echojs.com/');
+			result.idn.should.equal('echojs.com/');
 
-            parseInt(result.rank).should.be.a.Number;
-            parseInt(result.reach).should.be.a.Number;
+			parseInt(result.rank).should.be.a.Number;
+			parseInt(result.reach).should.be.a.Number;
 
-            done();
-        });
-    });
+			done();
+		});
+	});
 });
